34e55959ecdafa7c9cdec98f0f92948ade4908cf
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-10 22:48:16 +0000

    nomor 1

---
+++ bantul.js
@@ -0,0 +1,29 @@
+// 1. Buatlah sebuah variabel yang bernama listDesaBantul yang betipe data Array 
+// Multidemensi. Berisikan daftar desa di daerah bantul yang diambil dari 
+// https://id.wikipedia.org/wiki/Kategori:Desa_di_Kabupaten_Bantul. Ambil A - C.
+
+        // A
+        // Argodadi, Sedayu, Bantul
+        // Argomulyo, Sedayu, Bantul
+        // Argorejo, Sedayu, Bantul
+        // Argosari, Sedayu, Bantul
+        // B
+        // Bangunjiwo, Kasihan, Bantul
+        // Banguntapan, Banguntapan, Bantul
+        // Bantul, Bantul, Bantul
+        // Baturetno, Banguntapan, Bantul
+        // Bawuran, Pleret, Bantul
+        // C
+        // Canden, Jetis, Bantul
+        // Caturharjo, Pandak, Bantul
+        // Cetan Srigading
+
+
+
+var listDesaBantul = [
+    ['Argodadi', 'Argomulyo', 'Argorejo', 'Argosari'],
+    ['Bangunjiwo', 'Banguntapan', 'Bantul', 'Baturetno', 'Bawuran'],
+    ['Canden', 'Caturharjo', 'Cetan'],
+]
+
+console.log(listDesaBantul[1][2])
